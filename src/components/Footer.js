48ea69b527c5db29bd3c2c3f35f073101ee6d2ef
Anastasiia Sukhotska <v1nastyav1@gmail.com>
2022-05-05 11:35:05 +0200

    corrected mistakes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css';
 export function Footer() {
     return (
         <footer>
-            <p>React</p>
-            <p>Mozila Developer Network</p>
-            <p>My GitHub Profile</p>
-            <p>2022, Odessa, Ukraine</p>
+            <span>React</span>
+            <span>Mozila Developer Network</span>
+            <span>My GitHub Profile</span>
+            <span>2022, Odessa, Ukraine</span>
         </footer>
     )
 } 
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,11 @@ import './Main.css';
 
 export function Main () {
     return (
-        <div>
+        <main>
             <p>LoremLorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, debitis minima natus, maxime maiores quis voluptas quas itaque iste officia libero, voluptate fugit sit laboriosam porro eligendi? Harum corporis unde, natus maiores earum nemo aliquam soluta molestiae optio sed nulla, alias esse perspiciatis qui atque, rerum excepturi? Quidem, sapiente commodi.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, debitis minima natus, maxime maiores quis voluptas quas itaque iste officia libero, voluptate fugit sit laboriosam porro eligendi? Harum corporis unde, natus maiores earum nemo aliquam soluta molestiae optio sed nulla, alias esse perspiciatis qui atque, rerum excepturi? Quidem, sapiente commodi.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, debitis minima natus, maxime maiores quis voluptas quas itaque iste officia libero, voluptate fugit sit laboriosam porro eligendi? Harum corporis unde, natus maiores earum nemo aliquam soluta molestiae optio sed nulla, alias esse perspiciatis qui atque, rerum excepturi? Quidem, sapiente commodi.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, debitis minima natus, maxime maiores quis voluptas quas itaque iste officia libero, voluptate fugit sit laboriosam porro eligendi? Harum corporis unde, natus maiores earum nemo aliquam soluta molestiae optio sed nulla, alias esse perspiciatis qui atque, rerum excepturi? Quidem, sapiente commodi.</p>
-        </div>
+        </main>
     );
 }
